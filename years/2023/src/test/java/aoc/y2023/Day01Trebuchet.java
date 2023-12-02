7e49fcb58c34e194c08e935ecb84c120f8a1472d
Dave Ankin <daveankin@gmail.com>
2023-12-02 00:59:04 -0500

    '23 day1.2

--- years/2023/src/test/java/aoc/y2023/Day01Trebuchet.java
+++ years/2023/src/test/java/aoc/y2023/Day01Trebuchet.java
@@ -4,14 +4,13 @@ import lombok.SneakyThrows;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -60,5 +59,76 @@ class Day01Trebuchet {
 
         int sum = calibrationValues(input).stream().mapToInt(Integer::intValue).sum();
         assertEquals(54644, sum);
+    }
+
+    int calibrationValueSpelledOut(String line) {
+        Pattern spelledOut = Pattern.compile("(one|two|three|four|five|six|seven|eight|nine|\\d)");
+        var mapping = Map.of(
+                "one", "1",
+                "two", "2",
+                "three", "3",
+                "four", "4",
+                "five", "5",
+                "six", "6",
+                "seven", "7",
+                "eight", "8",
+                "nine", "9"
+        );
+        
+        Matcher matcher = spelledOut.matcher(line);
+        if (!matcher.find()) throw new IllegalArgumentException(line);
+        String first = mapping.getOrDefault(matcher.group(1), matcher.group(1));
+        
+        int from = line.length();
+        String found = null;
+        while (from-- > 0) {
+            if (matcher.find(from)) {
+                found = mapping.getOrDefault(matcher.group(1), matcher.group(1));
+                break;
+            }
+        }
+
+        String last = Objects.requireNonNullElse(found, first);
+        return ((first.charAt(0) - '0') * 10) + (last.charAt(0) - '0');
+    }
+
+    // https://jactl.io/blog/2023/12/02/advent-of-code-2023-day1.html
+    @ParameterizedTest
+    @CsvSource({
+            "xoney3zfour2z,12",
+            "x3zatwoneyz,31",
+            "threeight,38",
+    })
+    void test_calibrationValueSpelledOut(String input, int expected) {
+        assertEquals(expected, calibrationValueSpelledOut(input));
+    }
+    
+    List<Integer> calibrationValuesSpelledOut(String lines) {
+        return Arrays.stream(lines.split("\\s"))
+                .filter(Predicate.not(String::isEmpty))
+                .map(this::calibrationValueSpelledOut).toList();
+    }
+    
+    @Test
+    void test_calibrationValuesSpelledOut() {
+        String input = """
+                two1nine
+                eightwothree
+                abcone2threexyz
+                xtwone3four
+                4nineeightseven2
+                zoneight234
+                7pqrstsixteen
+                """;
+        assertEquals(List.of(29, 83, 13, 24, 42, 14, 76), calibrationValuesSpelledOut(input));
+        assertEquals(281, calibrationValuesSpelledOut(input).stream().mapToInt(Integer::intValue).sum());
+    }
+
+    @SneakyThrows
+    @Test
+    void submit_calibrationValuesSpelledOut() {
+        String input = IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/p1.calibration-values.txt")), StandardCharsets.UTF_8);
+        System.out.println(calibrationValuesSpelledOut(input));
+        System.out.println(calibrationValuesSpelledOut(input).stream().mapToLong(Integer::longValue).sum());
     }
 }
