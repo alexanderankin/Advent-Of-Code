51157dae7e45c1850f8503bbc2599c2ea0417fe8
Dave Ankin <daveankin@gmail.com>
2023-12-02 08:12:13 -0500

    '22 day6.2

--- years/2022/src/test/java/Day06Tuning.java
+++ years/2022/src/test/java/Day06Tuning.java
@@ -18,9 +18,16 @@ class Day06Tuning {
         return IOUtils.toString(Objects.requireNonNull(getClass().getResourceAsStream("/p6.1.txt")), StandardCharsets.UTF_8);
     }
 
-    int findMarker(String message) {
+    int findMarkerPacket(String message) {
+        return findMarker(message, 4);
+    }
+
+    int findMarkerMessage(String message) {
+        return findMarker(message, 14);
+    }
+
+    int findMarker(String message, int upTo) {
         var chars = message.toCharArray();
-        var upTo = 4;
 
         LinkedHashSet<Character> c = new LinkedHashSet<>();
         for (int i = 0, length = chars.length; i < length; i++) {
@@ -51,11 +58,28 @@ class Day06Tuning {
             "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw,11",
     })
     void test_findMarker(String message, int expected) {
-        assertEquals(expected, findMarker(message));
+        assertEquals(expected, findMarkerPacket(message));
     }
 
     @Test
     void submit_findMarker() {
-        assertEquals(1542, findMarker(readValue()));
+        assertEquals(1542, findMarkerPacket(readValue()));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "mjqjpqmgbljsphdztnvjfqwrcgsmlb,19",
+            "bvwbjplbgvbhsrlpgdmjqwftvncz,23",
+            "nppdvjthqldpwncqszvftbrmjlhg,23",
+            "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg,29",
+            "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw,26",
+    })
+    void test_findMarkerMessage(String message, int expected) {
+        assertEquals(expected, findMarkerMessage(message));
+    }
+
+    @Test
+    void submit_findMarkerMessage() {
+        assertEquals(3153, findMarkerMessage(readValue()));
     }
 }
